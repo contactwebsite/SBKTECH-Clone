b1d970dade6fede5eb66cf8f9a64d69da6c07d02
contact.website0p <contact.website0p@gmail.com>
2026-04-27 23:10:49 +0000

    fix BASE_URL in blog slug page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { getBlogsFromGitHub } from '@/lib/github';
 
 export const dynamic = 'force-dynamic';
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://balencia-pr.vercel.app'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://balencia.ma'
 
 export default async function BlogPage() {
   const blogs = await getBlogsFromGitHub();
